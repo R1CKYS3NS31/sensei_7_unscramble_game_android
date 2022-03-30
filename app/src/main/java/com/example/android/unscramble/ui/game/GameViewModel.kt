7608f14ed088d72a45a7f72d049499dff44b525c
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-31 02:35:14 +0300

    talk back commit

--- app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -1,13 +1,17 @@
 package com.example.android.unscramble.ui.game
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.TtsSpan
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 
 /**
-* ViewModel containing the app data and methods to process the data
-*/
+ * ViewModel containing the app data and methods to process the data
+ */
 class GameViewModel : ViewModel() {
 
     // Declare another public immutable field and override its getter method.
@@ -23,8 +27,24 @@ class GameViewModel : ViewModel() {
         get() = _currentWordCount
 
     private val _currentScrambledWord = MutableLiveData<String>()
-    val currentScrambledWord: LiveData<String>
-        get() = _currentScrambledWord
+    val currentScrambledWord: LiveData<Spannable> = Transformations.map(
+        _currentScrambledWord
+    ) {
+        if (it == null) {
+            SpannableString("")
+        } else {
+            val scrambledWord = it.toString()
+            val spannable: Spannable = SpannableString(scrambledWord)
+            spannable.setSpan(
+                TtsSpan.VerbatimBuilder(scrambledWord).build(),
+                0,
+                scrambledWord.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            spannable
+        }
+    }
+//        get() = _currentScrambledWord
 
     // List of words used in the game
     private val wordsList: MutableList<String> = mutableListOf()
@@ -64,9 +84,9 @@ class GameViewModel : ViewModel() {
     /*
     * Re-initializes the game data to restart the game.
     */
-    fun reinitializeData(){
-        _score.value=0
-        _currentWordCount.value=0
+    fun reinitializeData() {
+        _score.value = 0
+        _currentWordCount.value = 0
         wordsList.clear()
         getNextWord()
     }
@@ -89,6 +109,7 @@ class GameViewModel : ViewModel() {
         }
         return false
     }
+
     /*
     * Returns true if the current word count is less than MAX_NO_OF_WORDS.
     * Updates the next word.
