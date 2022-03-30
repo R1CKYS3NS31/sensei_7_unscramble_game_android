f719c6ce162f41eb3b6abc0a8dfd3e6c645c860c
DERRCIKSONNDAMBUKI <derricksonndambuki@gmail.com>
2022-03-31 01:41:33 +0300

    DataBinding commit

--- app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
+++ app/src/main/java/com/example/android/unscramble/ui/game/GameFragment.kt
@@ -21,6 +21,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.android.unscramble.R
@@ -46,7 +47,8 @@ class GameFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         // Inflate the layout XML file and return a binding object instance
-        binding = GameFragmentBinding.inflate(inflater, container, false)
+//        binding = GameFragmentBinding.inflate(inflater, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.game_fragment, container, false)
         Log.d("GameFragment", "GameFragment/re-created!")
         Log.d(
             "GameFragment", "Word: ${viewModel.currentScrambledWord}" +
@@ -73,6 +75,14 @@ class GameFragment : Fragment() {
             viewLifecycleOwner
         ) { newWord ->
             binding.textViewUnscrambledWord.text = newWord
+        }
+        viewModel.currentWordCount.observe(
+            viewLifecycleOwner
+        ) { newWordCount ->
+            binding.wordCount.text = getString(
+                R.string.word_count, newWordCount,
+                MAX_NO_OF_WORDS
+            )
         }
     }
 
